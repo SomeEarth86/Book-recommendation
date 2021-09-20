9447ddff91a52da462dc79672b70b9c0a6559736
Samarth Krishna Joshi <skrishnaj86@gmail.com>
2021-09-20 09:53:13 +0530

    description added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import React, { useState } from "react";
 
 const bookLibrary = {
   NonFicton: [
-    { name: "Atomic Habits", rating: "5/5" },
-    { name: "The Subtle Art of Not giving a f*ck", rating: "4/5" },
-    { name: "Mindset", rating: "3.5/5" }
+    { name: "Atomic Habits", rating: "Author: James Clear  |    5/5" },
+    {
+      name: "The Subtle Art of Not giving a f*ck",
+      rating: "Author: Mark Manson   |  4/5"
+    },
+    { name: "Mindset", rating: "Author: Carol Dweck | 3.5/5" }
   ],
 
   Fiction: [
-    { name: "Harry Potter Series", rating: "4/5" },
-    { name: "The Song of Ice and Fire", rating: "4.5/5" },
-    { name: "Ram Chandra Series", rating: "3/5" }
+    { name: "Harry Potter Series", rating: "Author: J.K. Rowling   |   4/5" },
+    {
+      name: "The Song of Ice and Fire",
+      rating: "Author: G.R.R Martin  |  4.5/5"
+    },
+    { name: "Ram Chandra Series", rating: "Author: Amish Tripathi  |  3/5" }
   ],
 
   Finance: [
-    { name: "Rich Dad and Poor Dad", rating: "4/5" },
-    { name: "The Psychology of Money", rating: "4.5/5" },
-    { name: "The Intelligent Investor", rating: "3.5/5" }
+    { name: "Rich Dad and Poor Dad", rating: "Author: Robert Kiyosaki  | 4/5" },
+    {
+      name: "The Psychology of Money",
+      rating: "Author: Morgan Housel | 4.5/5"
+    },
+    {
+      name: "The Intelligent Investor",
+      rating: "Author: Benjamin Graham | 3.5/5"
+    }
   ]
 };
 export default function App() {
